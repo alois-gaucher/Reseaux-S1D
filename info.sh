01f93139a1428c5eb6a83eec43bd3cb91b974cd5
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2019-05-20 16:10:17 +0200

    Exercice du TP7

--- info.sh
+++ info.sh
@@ -3,28 +3,27 @@
 # Usage : ./info.sh
 # Aloïs GAUCHER
 
-while [[ $nom == '' ]]
-do
-	echo "Quel est votre nom?"
-	read nom
-done
-
-while [[ $prenom == '' ]]
-do
-	echo "Quel est votre prenom?"
-	read prenom
-done
-
-while [[ $vmid == '' ]]
-do
-	echo "Quel est votre vmid?"
-	read vmid
-done
-
 if [ -f vmid.txt ]
 then
 	echo 'Le fichier vmid.txt existe déjà!'
 	exit
 else
+	while [[ $nom == '' ]]
+	do
+		echo "Quel est votre nom?"
+		read nom
+	done
+
+	while [[ $prenom == '' ]]
+	do
+		echo "Quel est votre prenom?"
+		read prenom
+	done
+
+	while [[ $vmid == '' ]]
+	do
+		echo "Quel est votre vmid?"
+		read vmid
+	done
 	printf "%s\n" "VMID $vmid" "NOM: $nom" "PRENOM: $prenom" > vmid.txt
 fi
