373fa846f57ed5f291f920db23e347f88f8afa20
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2019-05-28 14:13:03 +0000

    Bêta 1

---
+++ autofstab.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script Exercice TP 08 S1
+# Usage : ./autofstab.sh
+# Aloïs GAUCHER
+
+if [ -f /etc/fstab ]
+then
+	echo 'Le fichier /etc/fstab existe, super!'
+	while [[ $vmid == '' ]]
+	do
+		echo "Quel est le VMID de la machine que vous voulez ajouter? (Syntaxe: VM.ID)"
+		read vmid
+		echo $vmid
+	done
+	$ipvmid = "172.16."$vmid
+	echo $ipvmid
+	mkdir /partage$vmid && chmod 777 /partage$vmid
+	printf "%s" "$vmid:/partage$vmid /partage$vmid nfs defaults,user,auto 0 0" > /etc/fstab
+	echo 'Le serveur a bien été ajouté!'
+else
+	exit
+fi
